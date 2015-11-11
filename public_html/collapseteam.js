6c991030609491127a7ac0f9b39d1db7313e8430
patbeagan1 <pbeagan1@gmail.com>
2015-11-11 15:48:09 -0500

    converting the collapseteam.js page

---
+++ public_html/collapseteam.js
@@ -0,0 +1,162 @@
+document.write('\
+<collapse id="collapseteam">\
+\
+<h1>Team Meetings</h1>\
+        <p>\
+        Team meetings will be held as needed - they will appear on the calendar when we decide to meet.\
+        <br>Meetings will last about an hour and be held either on Tuesdays at 7 or Thursdays at 630.</p>\
+        <br>\
+<h1>Team Project Assignments</h1>\
+        <table border=1>\
+        <tr> <td>Project</td> <td>Subproject</td> <td>Team Assigned</td>\
+                </tr><tr> <td>Application</td> <td>User Auth</td> <td>Blue</td>\
+                </tr><tr> <td></td> <td>Data Entry</td> <td><strong>Red</strong></td>\
+                </tr><tr> <td></td> <td>Reporting and Search</td> <td>Blue</td>\
+                </tr><tr> <td></td> <td>Mapping</td> <td><strong>Red</strong></td>\
+                </tr><tr> <td>Tablet</td> <td>User Auth</td> <td>Orange</td>\
+                </tr><tr> <td></td> <td>Data Entry (Forms)</td> <td>Green</td>\
+                </tr><tr> <td></td> <td>Data Entry (GIS)</td> <td>Blue</td>\
+                </tr><tr> <td></td> <td>Form Printing</td> <td><strong>Red</strong></td>\
+                </tr><tr> <td></td> <td>Browsing and Search</td> <td>Green</td>\
+                </tr><tr> <td></td> <td>Delayed Upload</td> <td>Green</td>\
+                </tr><tr> <td>Database</td> <td>Basic</td> <td>Orange</td>\
+                </tr><tr> <td></td> <td>Federation</td> <td>Orange</td>\
+        </tr>\
+</table>\
+<br>\
+\
+<h1>Group Role Assignment</h1>\
+<table>\
+<tr><td>\
+        <table border=1>\
+                <tr>\
+        <td><strong>RED</strong></td>\
+        <td>Application<br>Mapping</td>\
+        <td>Application<br>Data Entry</td>\
+        <td>Tablet<br>Form Printing</td>\
+        </tr>\
+                <tr>\
+                <td><h3>Manager</h3></td>\
+                <td>Alexey<br>(Shablygin)</td>\
+                <td>James<br>(Desjardin)</td>\
+                <td>Mike<br>(Vartanian)</td>\
+                </tr>\
+                <tr>\
+                <td><h3>Members</h3></td>\
+                <td>Aaron<br>Jake<br>Ben<br></td>\
+                <td>Michael ZT<br>Frank</td>\
+                <td>Yucheng<br>Nathan</td>\
+                </tr>\
+        </table>\
+    <br>\
+\
+</td><td>\
+  \
+  <table border=1>\
+                <tr>\
+        <td><strong>ORANGE</strong></td>\
+        <td>Tablet<br>User Auth</td>\
+        <td>Database<br>Basic</td>\
+        <td>Database<br>Federation</td>\
+        </tr>\
+                <tr>\
+                <td><h3>Manager</h3></td>\
+                <td>Dhonovan<br>(Hausernam)</td>\
+                <td>Dan<br>(Chu)</td>\
+                <td>Tim<br>(Wilder)</td>\
+                </tr>\
+                <tr>\
+                <td><h3>Members</h3></td>\
+                <td>Eric<br>George<br></td>\
+                <td>Lucas<br>Jared</td>\
+                <td>Kavya<br>Dominic<br>Tyler</td>\
+                </tr>\
+        </table>\
+    \
+        <br>\
+\
+</td></tr><tr><td>\
+\
+  <table border=1>\
+                <tr>\
+        <td><strong>BLUE</strong></td>\
+        <td>Application<br>User Auth</td>\
+        <td>Application<br>Recording and<br>Search</td>\
+        <td>Tablet<br>GIS entry</td>\
+        <td>User Interface</td>\
+        </tr>\
+                <tr>\
+                <td><h3>Members</h3></td>\
+                <td>Evan<br>Callum<br></td>\
+                <td>Karl<br>Sandeep</td>\
+                <td>Yuan</td>\
+                <td>Adam<br>Noel</td>\
+                </tr>\
+        <tr>\
+        <td><h3>Contact person</h3></td>\
+        <td>Callum</td>\
+        <td></td>\
+        <td></td>\
+        </tr>\
+        </table>\
+    \
+        <br>\
+</td><td>\
+    <table border=1>\
+                <tr>\
+        <td><strong>GREEN</strong></td>\
+        <td>Tablet<br>Data Entry</td>\
+        <td>Tablet<br>Browsing</td>\
+        <td>Tablet<br>Delayed Upload</td>\
+        </tr>\
+                <tr>\
+                <td><h3>Managers</h3></td>\
+                <td>Eric<br>(Maryea)</td>\
+                <td>Alex<br>(Sullivan)</td>\
+                <td>Anther<br>(Ahktar)</td>\
+                </tr>\
+        <tr>\
+        <td><h3>Members</h3></td>\
+                <td>Drew<br>Peter<br>Neven</td>\
+                <td>Shawn<br>Andrew</td>\
+                <td>Josh<br>Michael</td>\
+                </tr>\
+        </table>\
+        <br>\
+\
+</td>\
+</tr>\
+</table>\
+\
+\
+\
+        <h1>Presentations</h1>\
+                <table border=1>\
+                        <tr>\
+                        <td><h2>SRS<br>(10/27-10/29)</h2></td>\
+                        <td><h2>SDS<br>(11/17)</h2></td>\
+                        <td><h2>Final<br>(12/18)</h2></td>\
+                </tr>\
+                <tr>\
+                        <td>Frank<br>Mike V<br>Alexey<br></td>\
+                        <td>Michael ZT<br>Aaron<br>Yucheng<br></td>\
+                        <td>James<br>Jake<br>Ben<br>Nathan<br></td>\
+                </tr>\
+        </table>\
+<br>\
+<br>\
+<h1><u>Role Reponsibilities</u></h1>\
+        <h2>Team Manager</h2>\
+        <p>\
+        The team manager is responsible for organizing team meetings, communicating with the group managers and other team managers, keeping the team informed and ensuring that assignments are both high quality and passed in on time.\
+        </p><br>\
+        <h2>Group Manager</h2>\
+        <p>\
+        The group manager is responsible for organizing group meetings, reporting progress to the team manager and completing high quality work.\
+        </p><br>\
+        <h2>Group Member</h2>\
+        <p>\
+        Group members are responsible for completing high quality work and communicating with group members from other teams as needed.\
+        </p><br>\
+        \
+');
